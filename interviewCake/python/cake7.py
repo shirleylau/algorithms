fe0625b1de1174e5634090f8978518cf67c072de
sllau <shirley.lau@renaissance.com>
2016-08-16 22:19:44 -0700

    7 done

--- interviewCake/python/cake7.py
+++ interviewCake/python/cake7.py
@@ -22,7 +22,6 @@ class temp_tracker():
         self.sum = 0
 
     def insert(self, temp):
-        self.temps[temp] += 1
         self.count += 1
         self.sum += temp
         # mode_count = temps[self.mode[0]] or None
@@ -33,15 +32,17 @@ class temp_tracker():
             # Check mode
             mode_count = self.temps[self.mode[0]]
             curr_count = self.temps[temp]
-            if curr_count == mode_count: # Add to mode
+            if curr_count + 1 == mode_count and not (temp in self.mode):
                 self.mode.append(temp)
-            elif curr_count > mode_count:
+            elif curr_count == mode_count:
                 self.mode = [temp]
 
             if temp < self.min: # Check Min
                 self.min = temp
             elif temp > self.max: # Check Max
                 self.max = temp
+
+        self.temps[temp] += 1
 
     def get_max(self):
         print self.max
@@ -65,6 +66,7 @@ mine.insert(12)
 mine.insert(5)
 mine.insert(5)
 mine.insert(3)
+mine.insert(12)
 
 mine.get_max()
 mine.get_min()
